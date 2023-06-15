9ee2a18570de747f00d4700f45e37109b2a535f7
Jakub Hrášek <hrasekj@gmail.com>
2023-06-15 11:28:19 +0200

    feat: rework layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,5 +5,9 @@ export default async function Home() {
   const notionService = new NotionService();
   const posts = await notionService.getPublishedBlogPots();
 
-  return <PostList posts={posts} />;
+  return (
+    <main className="container relative px-4 md:px-0 max-w-6xl mx-auto -mt-32">
+      <PostList posts={posts} />
+    </main>
+  );
 }

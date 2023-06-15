fcb0131534d5ba1e3cecdc637d284c778f977c25
Jakub Hrášek <hrasekj@gmail.com>
2023-06-15 11:19:26 +0200

    feat: add styles to post list

--- src/components/post-list/PostList.tsx
+++ src/components/post-list/PostList.tsx
@@ -6,15 +6,22 @@ type Props = {
   posts: BlogPost[];
 };
 
-const PostList: FC<Props> = ({ posts }) => (
-  <div className="bg-gray-100 p-4">
-    <h2 className="text-2xl font-bold mb-4">Post List</h2>
-    <div className="flex flex-col gap-4">
-      {posts.map((post) => (
-        <SimplePost key={post.id} post={post} />
-      ))}
+const PostList: FC<Props> = ({ posts }) => {
+  const leadPost = posts.shift();
+
+  return (
+    <div className="w-full text-xl md:text-2xl text-gray-800 leading-normal rounded-t">
+      {/*lead post*/}
+      {leadPost && <SimplePost post={leadPost} />}
+
+      {/*posts container*/}
+      <div className="flex flex-wrap justify-between pt-12 -mx-6">
+        {posts.map((post) => (
+          <SimplePost key={post.id} post={post} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default PostList;

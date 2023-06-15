69972c86c0cbf4890be0e2ce0e5690a586821c2e
Jakub Hrášek <hrasekj@gmail.com>
2023-06-15 11:19:11 +0200

    feat: add styles to simple post

--- src/components/simple-post/SimplePost.tsx
+++ src/components/simple-post/SimplePost.tsx
@@ -1,18 +1,38 @@
 import { BlogPost } from '@/types/schema';
+import Link from 'next/link';
 import { FC } from 'react';
 import ImageWithShimmer from '../images/ImageWithShimmer';
+import SimplePostFooter from './SimplePostFooter';
+import TagList from './TagList';
 
 type Props = {
   className?: string;
   post: BlogPost;
 };
 
-export const SimplePost: FC<Props> = ({ className, post }) => (
-  <div className={'bg-white rounded shadow mb-4 ' + (className ?? '')}>
-    <ImageWithShimmer src={post.cover} alt={post.title} width={500} height={300} className="rounded-t" />
-    <div className="p-4">
-      <h3 className="text-lg font-semibold mb-2">{post.title}</h3>
-      <p className="text-gray-700">{post.description}</p>
+export const SimplePost: FC<Props> = ({ post }) => (
+  <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink text-white">
+    <div className="flex-1 bg-gray-900  rounded-t overflow-hidden shadow-lg">
+      <ImageWithShimmer
+        className="h-64 w-full rounded-t pb-6"
+        src={post.cover}
+        alt={post.title}
+        width={800}
+        height={600}
+      />
+      <TagList tags={post.tags} />
+
+      <Link className="flex flex-wrap no-underline hover:no-underline" href="#">
+        <h3 className="w-full font-bold text-xl mt-6 px-6">{post.title}</h3>
+        <p className="font-serif text-base px-6 mb-5">{post.description}</p>
+      </Link>
+    </div>
+
+    <div className="bg-gray-900 flex-none mt-auto rounded-b overflow-hidden shadow-lg p-6">
+      <div className="flex items-center justify-between">
+        <SimplePostFooter />
+        <p className=" text-xs md:text-sm">1 MIN READ</p>
+      </div>
     </div>
   </div>
 );

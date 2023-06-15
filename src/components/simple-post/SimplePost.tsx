8b2fa04855ae16e0aee9d41b9cf1da5b382ddc95
Jakub Hrášek <hrasekj@gmail.com>
2023-06-15 09:40:39 +0200

    refactor: remove shared folder

--- src/components/simple-post/SimplePost.tsx
+++ src/components/simple-post/SimplePost.tsx
@@ -1,6 +1,6 @@
 import { BlogPost } from '@/types/schema';
-import Image from 'next/image';
 import { FC } from 'react';
+import ImageWithShimmer from '../images/ImageWithShimmer';
 
 type Props = {
   className?: string;
@@ -9,7 +9,7 @@ type Props = {
 
 export const SimplePost: FC<Props> = ({ className, post }) => (
   <div className={'bg-white rounded shadow mb-4 ' + (className ?? '')}>
-    <Image src={post.cover} alt={post.title} width={500} height={300} className="rounded-t" />
+    <ImageWithShimmer src={post.cover} alt={post.title} width={500} height={300} className="rounded-t" />
     <div className="p-4">
       <h3 className="text-lg font-semibold mb-2">{post.title}</h3>
       <p className="text-gray-700">{post.description}</p>
